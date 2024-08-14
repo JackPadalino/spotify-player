bbb32613ba693498dfae913682e4d88064894456
JackPadalino <pada0867@gmail.com>
2024-08-14 10:23:28 -0400

    able to display tracks and click to play

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-const trackIds = [
-  "1ErfPo4xsbhLL5wQcq4AIS",
-  "13DqJv37Zxd4cDgBV9bLJN",
-  "3B2tfZKZkelId6gTkm1xw5",
-  "1m85LGddnODvgCB2XmX8mW",
-  "5oQQY1Aah70hJb1FHq67sw",
-  "4mEtxdE7GjjOInVAXNcE1I",
-  "52O518ECDOaUthShbS6Kai",
-  "4WofQr7xeywxUxYqUqhgGs",
-  "2Qg4N0TJVXhWXFPc9DDp9k",
-  "2iEKl6IzXbgyK4qlPo5G9H",
-];
-
-// logic for embedding a single track
-const trackId = trackIds[0];
-
-// Update the iframe's src attribute to play the specific track
-document.addEventListener("DOMContentLoaded", function () {
-  const iframe = document.querySelector("iframe");
-  iframe.src = `https://open.spotify.com/embed/track/${trackId}?utm_source=generator&theme=0`;
-});
-
 // requesting access tokens from spotify API
-
 const getAccessToken = () => {
   const url = "https://accounts.spotify.com/api/token";
-  const clientId = "ee45ce02d7d64ad7ba8ee9e4aee7d9c3";
-  const clientSecret = "549a0b7e22cb435fb779e430698494a4";
+  const clientId = "add your spotify client id here (as a string)";
+  const clientSecret = "add your spotify client secret key here (as a string)";
 
   const body = new URLSearchParams({
     grant_type: "client_credentials",
@@ -55,6 +32,8 @@ const getAccessToken = () => {
 };
 
 const getTrack = (query, accessToken) => {
+  // using the api endpoint to search for tracks
+  // this will return all tracks that match, or are similar to, the user's query
   const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
   return fetch(url, {
     method: "GET",
@@ -67,18 +46,69 @@ const getTrack = (query, accessToken) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      return data;
     })
     .catch((error) => {
       console.error("Error fetching token:", error);
     });
 };
 
-// Event listener for the submit button
+// getting list element from HTML
+const trackList = document.getElementById("trackList");
+
+// event listener for the submit button
 document.getElementById("submitBtn").addEventListener("click", function () {
+  // clear the list before adding new tracks
+  while (trackList.firstChild) {
+    trackList.removeChild(trackList.firstChild);
+  }
+
+  // get the user's query from the input element
   const query = document.getElementById("inputString").value;
   getAccessToken().then((response) => {
-    getTrack(query, response.access_token);
+    getTrack(query, response.access_token).then((response) => {
+      response.tracks.items.forEach((track) => {
+        // create a new list item
+        const newTack = document.createElement("li");
+
+        // set the text of the list item to the current array element
+        newTack.textContent = `${track.name} - ${track.artists[0].name}`;
+        newTack.setAttribute("data-id", track.id);
+
+        // add the list item to the ul
+        trackList.appendChild(newTack);
+      });
+    });
   });
   document.getElementById("inputString").value = "";
+});
+
+// event listener to play song and add iFrame to HTML
+trackList.addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    const trackId = event.target.getAttribute("data-id");
+
+    // create the iframe element
+    const iframe = document.createElement("iframe");
+
+    // set the iframe attributes with spotify information (from the documentation)
+    iframe.title = "Spotify Embed: Single Track";
+    iframe.width = "100%";
+    iframe.height = "100%";
+    iframe.style.minHeight = "360px";
+    iframe.frameBorder = "0";
+    iframe.allow =
+      "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
+    iframe.loading = "lazy";
+    iframe.src = `https://open.spotify.com/embed/track/${trackId}?utm_source=generator&theme=0`;
+
+    // get the iframe container div
+    const iframeContainer = document.getElementById("iframeContainer");
+
+    // clear any previous iframe
+    iframeContainer.innerHTML = "";
+
+    // add the new iframe to the placeholder container in the HTML
+    iframeContainer.appendChild(iframe);
+  }
 });
